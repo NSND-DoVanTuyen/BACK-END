a3d9d517b67cef721f91d2bd1386ee437fe5a299
DoTuyen <162678672+NSND-DoVanTuyen@users.noreply.github.com>
2025-04-11 20:26:31 +0700

    Add files via upload

---
+++ Tuan1/check_so_tuoi.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Tuan1
+{
+  
+    public class CheckAmDuongException : Exception
+    {
+      
+        public CheckAmDuongException(string message) : base(message)
+        {
+            
+        }
+    }
+}
